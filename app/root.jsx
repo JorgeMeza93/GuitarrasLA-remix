05f63468b1be8b3b67fc711a1cde26966f6ea872
Jorge Meza Bello <jorberiver@gmail.com>
2022-11-06 20:21:06 -0600

    Comienzo del proyecto

---
+++ app/root.jsx
@@ -0,0 +1,43 @@
+import { Meta, Links } from "@remix-run/react";
+import styles from "./styles/index.css";
+
+export function meta(){
+    return(
+        {
+            charset: "utf-8",
+            title: "GuitarLA remix",
+            viewport: "width=device-width,initial-scale=1"
+        }
+    )
+}
+export function links(){
+    return [
+        {
+            rel: "stylesheet",
+            href: styles
+        }
+    ]
+}
+
+
+export default function App(){
+    return (
+        <Document>
+            <h1>Hola Mundo</h1>
+        </Document>
+    )
+}
+
+function Document({children}){
+    return (
+        <html lang="es">
+            <head>
+               <Meta/>
+               <Links/>
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
