4db2cfe1168976a325b7ccf2ed564514980ebf98
Jorge Meza Bello <jorberiver@gmail.com>
2022-11-10 20:49:19 -0600

    Cambios hechos en tienda

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,16 +1,50 @@
-import React from 'react'
+import React from 'react';
+import imagen from "../../public/img/nosotros.jpg";
+import styles from "../styles/nosotros.css";
 
 export function meta(){
   return (
     {
-      title: "GuitarLA - Nosotros"
+      title: "GuitarLA - Nosotros",
+      description: "Venta de guitarras, blog de música"
     }
   )
+}
+export function links(){
+  return [
+    {
+      rel: "stylesheet",
+      href: styles
+    },
+    {
+      rel: "preload",
+      href: imagen,
+      as: "image"
+    }
+  ]
 }
 
 const Nosotros = () => {
   return (
-    <div>nosotros</div>
+   <main className='contenedor nosotros'>
+    <h2 className='heading'>Nosotros</h2>
+    <div className='contenido'>
+      <img src={imagen} alt="Imagen Guitarra" />
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas malesuada eu arcu quis placerat. Suspendisse non elit semper, lobortis elit at, semper justo. Mauris tempor elit vel ligula pretium vehicula. 
+          Ut sollicitudin augue eget dignissim condimentum. Morbi mauris eros, dictum ut vulputate at, fermentum vitae nulla. Donec luctus dui mollis erat egestas, eget tristique ex pharetra. Pellentesque vitae rhoncus
+          ex, non bibendum lorem. Phasellus non dapibus arcu, vel viverra ipsum. Phasellus mollis imperdiet ante vel luctus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas malesuada eu arcu quis placerat. Suspendisse non elit semper, lobortis elit at, semper justo. Mauris tempor elit vel ligula pretium vehicula. 
+          Ut sollicitudin augue eget dignissim condimentum. Morbi mauris eros, dictum ut vulputate at, fermentum vitae nulla. Donec luctus dui mollis erat egestas, eget tristique ex pharetra. Pellentesque vitae rhoncus
+          ex, non bibendum lorem. Phasellus non dapibus arcu, vel viverra ipsum. Phasellus mollis imperdiet ante vel luctus.
+          Maecenas malesuada eu arcu quis placerat. Suspendisse non elit semper, lobortis elit at, semper justo. Mauris tempor elit vel ligula pretium
+        </p>
+      </div>
+    </div>
+   </main>
   )
 }
 
--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+export async function loader(){
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`)
+  const resultado = await respuesta.json();
+  return{}
+}
+
 const Tienda = () => {
   return (
     <div>tienda</div>
