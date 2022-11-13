d1f40604f8d12d7b0aa24a6347cfd372620a8bd7
Jorge Meza Bello <jorberiver@gmail.com>
2022-11-12 20:33:21 -0600

    Entradas del blog listas

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import { useLoaderData } from '@remix-run/react';
 import Post from '~/components/post';
+import styles from "../styles/blog.css";
+
+export function links(){
+  return [
+    {
+      rel: "stylesheet",
+      href: styles
+    }
+  ]
+}
 
 export async function getPosts(){
   const respuesta = await fetch(`${process.env.API_URL}/posts?populate=imagen`);
