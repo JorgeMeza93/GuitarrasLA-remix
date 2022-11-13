adbc7a1c46be7e6fdefd05d32638f3d8fc3296c3
Jorge Meza Bello <jorberiver@gmail.com>
2022-11-12 21:47:33 -0600

    Blog listo

--- app/components/post.jsx
+++ app/components/post.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from '@remix-run/react';
+import { formatearFecha } from '~/helpers/helpers';
 
 const Post = ({ post }) => {
   const { contenido, imagen, titulo, url, publishedAt } = post;
@@ -8,7 +9,7 @@ const Post = ({ post }) => {
         <img src={imagen.data.attributes.formats.small.url} alt={`imagen de ${titulo}`} className="imagen" />
         <div className='contenido'>
             <h3>{titulo}</h3>
-            <p className='fecha'>{publishedAt}</p>
+            <p className='fecha'>{formatearFecha(publishedAt)}</p>
             <p className='resumen'>{contenido}</p>
             <Link className="enlace" to={`/posts/${url}`}>Leer Post</Link>
         </div>
